15d2e121396da4c4b82da26fa406fd76b1246419
Jacob <julrich@gmail.com>
2020-06-12 15:10:43 -0700

    Add configuration to build scripts

--- build-pi.bat
+++ build-pi.bat
@@ -5,5 +5,5 @@ go build
 set GOOS=
 set GOARCH=
 set GOARM=
-scp -r twitch-caster static pi@raspberrypi:
+scp -r twitch-caster configuration.json static pi@raspberrypi:
 del twitch-caster
--- build-pi.sh
+++ build-pi.sh
@@ -1,3 +1,3 @@
 GOOS=linux GOARCH=arm GOARM=5 go build
-scp -r twitch-caster static pi@raspberrypi:
+scp -r twitch-caster configuration.json static pi@raspberrypi:
 rm twitch-caster
